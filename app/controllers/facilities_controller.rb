b1dc5c096f021c0b5f524c021b29b4843e18091e
ufuksaylan <66475538+ufuksaylan@users.noreply.github.com>
2025-03-31 14:31:53 +0300

    allow request from any origin

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -1,16 +1,43 @@
+# app/controllers/facilities_controller.rb
 class FacilitiesController < ApplicationController
-  FILTER_PARAMS = %i[facility_type city postcode wheelchair_accessible has_emergency specialization].freeze
+  FILTER_PARAMS = %i[
+    facility_type city postcode wheelchair_accessible has_emergency specialization
+    latitude longitude distance
+  ].freeze
 
   def index
     @facilities = Facility.all
 
-    filter_params(params).each do |key, value|
-      if %w[true false].include?(value.to_s.downcase) && Facility.column_for_attribute(key).type == :boolean
-        @facilities = @facilities.where(key => value.to_s.downcase == 'true')
+    standard_filters = filter_params(params).except(:latitude, :longitude, :distance)
+    standard_filters.each do |key, value|
+      next unless FILTER_PARAMS.include?(key.to_sym)
+
+      if Facility.column_for_attribute(key).type == :boolean
+        bool_value = value.to_s.downcase == 'true'
+        @facilities = @facilities.where(key => bool_value)
       elsif value.present?
         @facilities = @facilities.where(key => value)
       end
     end
+
+    lat_param = params[:latitude].presence
+    lng_param = params[:longitude].presence
+
+    if lat_param && lng_param
+      begin
+        lat_f = Float(lat_param)
+        lng_f = Float(lng_param)
+
+        point_text = "POINT(#{lng_f} #{lat_f})"
+        point_geography_sql = Arel.sql("ST_SetSRID(ST_GeomFromText('#{point_text}'), 4326)::geography")
+
+        @facilities = @facilities.order(Arel.sql("ST_Distance(location, #{point_geography_sql}) ASC"))
+
+      rescue ArgumentError, TypeError
+        Rails.logger.warn("Invalid latitude/longitude parameters received: lat=#{lat_param}, lng=#{lng_param}. Skipping distance sort.")
+      end
+    end
+
   end
 
   private
